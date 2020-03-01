874101391001e1dfb74093a56bb517936749a2e3
Ashwin <ashwyn222@gmail.com>
2020-03-01 21:58:08 +0530

    Added dummy server to provide test data

--- src/components/Tile.js
+++ src/components/Tile.js
@@ -4,6 +4,7 @@ export default class Tile extends Component {
     render() {
         return (
             <div className="tile">
+                <h4>{this.props.title}</h4>
                 <div>Min Temp: {this.props.minTemp}</div>
                 <div>Temp: {this.props.temp}</div>
                 <div>Max Temp: {this.props.maxTemp}</div>
--- src/components/WeatherDetails.js
+++ src/components/WeatherDetails.js
@@ -5,29 +5,56 @@ import axios from 'axios';
 export default class WeatherDetails extends Component {
     constructor() {
         super();
+        //http://samples.openweathermap.org/data/2.5/weather?id=${this.props.cityId}&appid=2dbe93d485a7fc29365445a9669b0e72
         this.state = {
-            weatherInfo: []
+            yesterday: [],
+            today: [],
+            tomorrow: []
         }
     }
 
 
     componentDidUpdate(prevProps) {
         if(prevProps.cityId !== this.props.cityId) {
-            axios.get(`http://samples.openweathermap.org/data/2.5/weather?id=${this.props.cityId}&appid=2dbe93d485a7fc29365445a9669b0e72`)
+            axios.get(`http://localhost:3001/yesterday`)
             .then(response => {
-                console.log(response.data);
-                this.setState({weatherInfo: response.data.main});
+                this.setState({yesterday: response.data});
+            });
+
+            axios.get(`http://localhost:3001/today`)
+            .then(response => {
+                this.setState({today: response.data});
+            });
+
+            axios.get(`http://localhost:3001/tomorrow`)
+            .then(response => {
+                this.setState({tomorrow: response.data});
             });
         }
     }
 
 
     render() {
-        const { temp_min, temp, temp_max } = this.state.weatherInfo; 
         return (
             <div className="content f">
-                <div>City Id: {this.props.cityId}</div>
-                <Tile minTemp={temp_min} temp={temp} maxTemp={temp_max}></Tile>
+                <Tile
+                    title={'Yesterday'}
+                    minTemp={this.state.yesterday.min_temp}
+                    temp={this.state.yesterday.temp}
+                    maxTemp={this.state.yesterday.max_temp}>
+                </Tile>
+                <Tile
+                    title={'Today'}
+                    minTemp={this.state.today.min_temp}
+                    temp={this.state.today.temp}
+                    maxTemp={this.state.today.max_temp}>
+                </Tile>
+                <Tile
+                    title={'Tomorrow'}
+                    minTemp={this.state.tomorrow.min_temp}
+                    temp={this.state.tomorrow.temp}
+                    maxTemp={this.state.tomorrow.max_temp}>
+                </Tile>
             </div>
         )
     }
